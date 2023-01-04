b553e1d218d2d73fdb1bbc2ba000d3483ce73f01
escorsi <patrickescorsi@gmail.com>
2023-01-04 08:27:40 -0300

    Adicionando testes e corrigindo arquivos do Docker

--- account/test/changeUserName.test.js
+++ account/test/changeUserName.test.js
@@ -2,5 +2,7 @@ import { changeUserNameUseCase } from "../src/use-case/changeUserName.js";
 import { createUserUseCase } from "../src/use-case/createUserAccount.js";
 
 createUserUseCase("Teste", "teste@email.com", "senhaTeste")
-console.log("Nome alterado com sucesso! \n");
+createUserUseCase("Alura", "alura@email.com", "senhaAlura")
 console.log(changeUserNameUseCase("teste@email.com", "Patrick"));
+console.log(changeUserNameUseCase("alura@email.com", "Sprint"));
+
--- account/test/searchUserAccountByState.test.js
+++ account/test/searchUserAccountByState.test.js
@@ -1,3 +1,14 @@
 import { searchUserAccountByState } from "../src/use-case/searchUserAccountByState.js";
+import { createUserUseCase } from "../src/use-case/createUserAccount.js";
+import { createUserAdress } from "../src/use-case/createUserAdress.js";
+
+createUserUseCase("Teste", "teste@email.com", "senhaTeste")
+createUserUseCase("Patrick", "teste2@email.com", "senhaTeste")
+createUserUseCase("AAAAA", "teste3@email.com", "senhaTeste")
+
+createUserAdress("teste2@email.com", "Rua 1", 123, "", "Liberdade", 1550000, "São Paulo", "SP");
+createUserAdress("teste3@email.com", "Rua 1", 123, "", "Vila Olinda", 1550000, "São Paulo", "SP");
+createUserAdress("teste@email.com", "Rua 2", 444, "", "Centro", 44444, "Curitiba", "PR");
+
 
 console.log(searchUserAccountByState("SP"));
