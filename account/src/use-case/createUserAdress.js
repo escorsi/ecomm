f96ad5ad1de01d7a38c56100eba7e506e9a470e4
escorsi <patrickescorsi@gmail.com>
2023-01-10 09:41:08 -0300

    Correção no arquivo createUserAdress.js

--- account/src/use-case/createUserAdress.js
+++ account/src/use-case/createUserAdress.js
@@ -8,11 +8,11 @@ export function createUserAdress(email, logradouro, numero, complemento, bairro,
         const index = accounts.findIndex((account) => account.email === email);
         accounts[index]["logradouro"] = logradouro;
         accounts[index]["numero"] = numero;
-        accounts[index]["complemento"] = complemento,
-        accounts[index]["bairro"] = bairro,
-        accounts[index]["cep"] = cep,
-        accounts[index]["cidade"] = cidade,
-        accounts[index]["uf"] = uf
+        accounts[index]["complemento"] = complemento;
+        accounts[index]["bairro"] = bairro;
+        accounts[index]["cep"] = cep;
+        accounts[index]["cidade"] = cidade;
+        accounts[index]["uf"] = uf;
 
         return accounts[index];
     }
