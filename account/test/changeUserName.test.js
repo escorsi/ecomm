4ca8944b5b66220f066933955a9d681afd999fdf
escorsi <patrickescorsi@gmail.com>
2023-03-24 18:01:54 -0300

    Removendo arquivos não mais necessários

--- account/test/changeUserName.test.js
+++
@@ -1,8 +0,0 @@
-import { changeUserNameUseCase } from "../src/use-case/changeUserName.js";
-import { createUserUseCase } from "../src/use-case/createUserAccount.js";
-
-createUserUseCase("Teste", "teste@email.com", "senhaTeste")
-createUserUseCase("Alura", "alura@email.com", "senhaAlura")
-console.log(changeUserNameUseCase("teste@email.com", "Patrick"));
-console.log(changeUserNameUseCase("alura@email.com", "Sprint"));
-
